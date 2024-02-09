bdb08eba0e3d7c7b139843ff22e0078c243453cf
jamest1206 <143458387+jamest1206@users.noreply.github.com>
2024-02-08 22:25:43 -0500

    Corrected Logo

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.accordionBox:first').addClass('active')
+    $('.accordionBox:first').children('.acc_trigger').children('i').addClass('fa-minus')
+    $('.accordionBox:first').addClass('active')
+});
